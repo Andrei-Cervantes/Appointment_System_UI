ed14f7afcc8bf8fdb59fb803379a47a70024e379
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-06-03 10:25:36 +0800

    chore: add react-scan for scanning unnecessary rerenders

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { scan } from "react-scan";
 import { ThemeProvider } from "./components/theme/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRoutes from "./routes/AppRoutes";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
 function App() {
+  useEffect(() => {
+    scan({
+      enabled: true,
+    });
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
