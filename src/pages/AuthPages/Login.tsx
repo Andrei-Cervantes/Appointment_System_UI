415176e6ee5d24b3fda8e8521f0b9dbe618141f4
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-06-04 10:44:21 +0800

    feat: add card component

--- src/pages/AuthPages/Login.tsx
+++ src/pages/AuthPages/Login.tsx
@@ -1,5 +1,22 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="h-full flex items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Input type="email" placeholder="Email" />
+          <Input type="password" placeholder="Password" />
+          <Button>Login</Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
